0be3a36621623a2a94e0e98be9aabb3263d94f85
381706-1VlasovAndrey <andrio.vlasov.2000@mail.ru>
2020-01-28 12:05:45 +0300

    add all

---
+++ Camera_control/CameraControl.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+  public float speed = 30.0f;
+    void Update()
+    {
+    if (Input.GetAxis("Horizontal") > 0) {
+      transform.Translate(transform.right * speed);
+    }
+    else if (Input.GetAxis("Horizontal") < 0) {
+      transform.Translate(-transform.right * speed);
+    }
+    if (Input.GetAxis("Vertical") > 0)
+    {
+      transform.Translate(transform.forward * speed);
+    }
+    else if (Input.GetAxis("Vertical") < 0)
+    {
+      transform.Translate(-transform.forward * speed);
+    }
+    if (Input.GetAxis("Jump") > 0)
+    {
+      transform.Translate(transform.up * speed);
+    }
+    if (Input.GetAxis("Jump") < 0)
+    {
+      transform.Translate(-transform.up * speed);
+    }
+  }
+}
